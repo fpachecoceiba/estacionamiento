24f6c027171e58a831ba35854a22533dc3c5c194
francisco.pacheco <francisco.pacheco@FRANCISCO-PACHECO.ceiba.local>
2019-04-08 17:10:36 -0500

    test

--- src/test/java/com/co/ceiba/parqueadero/pintegracion/VehiculoControllerTest.java
+++ src/test/java/com/co/ceiba/parqueadero/pintegracion/VehiculoControllerTest.java
@@ -2,9 +2,6 @@ package com.co.ceiba.parqueadero.pintegracion;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpEntity;
@@ -12,7 +9,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import com.co.ceiba.parqueadero.dominio.CarroDTO;
 //
